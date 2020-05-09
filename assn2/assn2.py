d94124e2ebc4a56dba76930127ddfc9e154504f4
kadynm49 <marsha38@wwu.edu>
2020-05-09 13:19:50 -0700

    time to report probabilities and generate sentences with trigrams

--- assn2/assn2.py
+++ assn2/assn2.py
@@ -8,6 +8,7 @@ from nltk.util import ngrams
 from collections import Counter
 import copy
 import random
+from math import log, exp
 # from IPython.display import clear_output
 
 start = " <s> "
@@ -33,7 +34,7 @@ for word in text_list.split():
         word_frequencies[word] = 1
 
 # Report size of vocabulary
-print("The vocabulary size is: " + str(len(word_frequencies)))
+print("\nVocabulary size: " + str(len(word_frequencies)) + "\n")
 
 # Get all tokens for ngrams
 tokens = []
@@ -73,38 +74,93 @@ for trigram in smoothed_trigrams:
     unsmoothed_trigrams[trigram] /= trigram_count
 
 
-# Generate Sentences using unigram model
-print("Unsmoothed Unigram Sentences:\n")
-# for i in range(10):
-#     sentence_length = random.randrange(10, 20)
-#     unigrams = unsmoothed_unigrams.most_common(sentence_length)
-#     sentence = "<s> "
-#     for j in range(sentence_length):
-#         if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
-#             sentence += str(unigrams[j][0][0]) + " "
-#     sentence += " </s>"
-#     print(sentence)
+# Generate Sentences using unsmoothed unigram model
+print("Unsmoothed Unigram Sentences:")
+for i in range(10):
+    sentence_length = random.randrange(10, 20)
+    unigrams = unsmoothed_unigrams.most_common(sentence_length)
+    sentence = "<s> "
+    for j in range(sentence_length):
+        if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
+            sentence += str(unigrams[j][0][0]) + " "
+    sentence += " </s>"
+    print(sentence)
 
-# print("\nSmoothed Unigram Sentences:")
-# for i in range(10):
-#     sentence_length = random.randrange(10, 20)
-#     unigrams = smoothed_unigrams.most_common(sentence_length)
-#     sentence = "<s> "
-#     for j in range(sentence_length):
-#         if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
-#             sentence += str(unigrams[j][0][0]) + " "
-#     sentence += " </s>"
-    # print(sentence)
-    
-    
-# Generate Sentences using Bigram model
+# Generate Sentences using unsmoothed unigram model
+print("\nSmoothed Unigram Sentences:")
+for i in range(10):
+    sentence_length = random.randrange(10, 20)
+    unigrams = smoothed_unigrams.most_common(sentence_length)
+    sentence = "<s> "
+    for j in range(sentence_length):
+        if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
+            sentence += str(unigrams[j][0][0]) + " "
+    sentence += " </s>"
+    print(sentence)
 
 # Create a list of sentence starts for unsmoothed bigrams
+# STORE PROBABILITIES TOOOOOOOOOOOO
 sentence_starters = []
-for bigram in unsmoothed_bigrams:
-    if(bigram[0] == "<s>"):
-        sentence_starters.append(bigram[1])
-print(sentence_starters)
-# for i in range(10):
-    
-            
+for bigram in unsmoothed_bigrams.most_common():
+    if bigram[0][0] == "<s>":
+        sentence_starters.append(bigram[0][1])
+    if len(sentence_starters) == 30:
+        break
+
+# Generate sentences using unsmoothed bigram model
+print("\nUnsmoothed Bigram Sentences:")
+for i in range(10):
+    current_word = sentence_starters[random.randrange(0,20)]
+    sentence = "<s> "
+    word_count = 0
+    while current_word != "</s>":
+        if word_count == 20:
+            break
+
+        sentence += current_word + " "
+        next_word = { "prob" : 0, "word": "" }
+
+        # Find most likely word to come after current word
+        for bigram in unsmoothed_bigrams:
+            # Set most likely word if this bigram has the highest probability found so far (25% chance it will not set)
+            if bigram[0] == current_word and unsmoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) == 1:
+                next_word["prob"] = unsmoothed_bigrams[bigram]
+                next_word["word"] = bigram[1]
+
+        current_word = next_word["word"]
+        word_count += 1
+    sentence += "</s>"
+    print(sentence)
+
+# Create a list of sentence starts for smoothed bigrams
+sentence_starters = []
+for bigram in smoothed_bigrams.most_common():
+    if bigram[0][0] == "<s>":
+        sentence_starters.append(bigram[0][1])
+    if len(sentence_starters) == 30:
+        break
+
+# Generate sentences using smoothed bigram model
+print("\nSmoothed Bigram Sentences:")
+for i in range(10):
+    current_word = sentence_starters[random.randrange(0,20)]
+    sentence = "<s> "
+    word_count = 0
+    while current_word != "</s>":
+        if word_count == 20:
+            break
+
+        sentence += current_word + " "
+        next_word = { "prob" : 0, "word": "" }
+
+        # Find most likely word to come after current word
+        for bigram in smoothed_bigrams:
+            # Set most likely word if this bigram has the highest probability found so far (25% chance it will not set)
+            if bigram[0] == current_word and smoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) > 1:
+                next_word["prob"] = smoothed_bigrams[bigram]
+                next_word["word"] = bigram[1]
+
+        current_word = next_word["word"]
+        word_count += 1
+    sentence += "</s>"
+    print(sentence)
