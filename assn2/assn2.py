c942613aeaf98776eaa86948c6c170c46fd582cb
Zephren de la Cerda <zephren123@gmail.com>
2020-05-09 14:49:02 -0700

    tag you're it

--- assn2/assn2.py
+++ assn2/assn2.py
@@ -80,11 +80,14 @@ for i in range(10):
     sentence_length = random.randrange(10, 20)
     unigrams = unsmoothed_unigrams.most_common(sentence_length)
     sentence = "<s> "
+    sentence_p = 0
     for j in range(sentence_length):
         if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
             sentence += str(unigrams[j][0][0]) + " "
+            sentence_p += log(unigrams[j][1])
     sentence += " </s>"
     print(sentence)
+    print("Sentence Probability (Log Space): ", sentence_p, "\n")
 
 # Generate Sentences using unsmoothed unigram model
 print("\nSmoothed Unigram Sentences:")
@@ -92,18 +95,20 @@ for i in range(10):
     sentence_length = random.randrange(10, 20)
     unigrams = smoothed_unigrams.most_common(sentence_length)
     sentence = "<s> "
+    sentence_p = 0
     for j in range(sentence_length):
         if (str(unigrams[j][0][0]) != "<s>" and str(unigrams[j][0][0]) != "</s>"):
             sentence += str(unigrams[j][0][0]) + " "
+            sentence_p += log(unigrams[j][1])
     sentence += " </s>"
     print(sentence)
+    print("Sentence Probability (Log Space): ", sentence_p, "\n")
 
 # Create a list of sentence starts for unsmoothed bigrams
-# STORE PROBABILITIES TOOOOOOOOOOOO
 sentence_starters = []
 for bigram in unsmoothed_bigrams.most_common():
     if bigram[0][0] == "<s>":
-        sentence_starters.append(bigram[0][1])
+        sentence_starters.append((bigram[0][1], bigram[1]))
     if len(sentence_starters) == 30:
         break
 
@@ -113,30 +118,34 @@ for i in range(10):
     current_word = sentence_starters[random.randrange(0,20)]
     sentence = "<s> "
     word_count = 0
-    while current_word != "</s>":
+    sentence_p = 0
+    while current_word[0] != "</s>":
         if word_count == 20:
             break
-
-        sentence += current_word + " "
+        sentence += current_word[0] + " "
+        if (current_word[1] != 0):
+            sentence_p += log(current_word[1])
         next_word = { "prob" : 0, "word": "" }
 
         # Find most likely word to come after current word
         for bigram in unsmoothed_bigrams:
             # Set most likely word if this bigram has the highest probability found so far (25% chance it will not set)
-            if bigram[0] == current_word and unsmoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) == 1:
+            if bigram[0] == current_word[0] and unsmoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) == 1:
                 next_word["prob"] = unsmoothed_bigrams[bigram]
                 next_word["word"] = bigram[1]
 
-        current_word = next_word["word"]
+        current_word = (next_word["word"], next_word["prob"])
         word_count += 1
     sentence += "</s>"
     print(sentence)
+    print("Sentence Probability (Log Space): ", sentence_p, "\n")
+    
 
 # Create a list of sentence starts for smoothed bigrams
 sentence_starters = []
 for bigram in smoothed_bigrams.most_common():
     if bigram[0][0] == "<s>":
-        sentence_starters.append(bigram[0][1])
+        sentence_starters.append((bigram[0][1], bigram[1]))
     if len(sentence_starters) == 30:
         break
 
@@ -146,21 +155,64 @@ for i in range(10):
     current_word = sentence_starters[random.randrange(0,20)]
     sentence = "<s> "
     word_count = 0
-    while current_word != "</s>":
+    sentence_p = 0
+    while current_word[0] != "</s>":
         if word_count == 20:
             break
 
-        sentence += current_word + " "
+        sentence += current_word[0] + " "
+        if (current_word[1] != 0):
+            sentence_p += log(current_word[1])
         next_word = { "prob" : 0, "word": "" }
 
         # Find most likely word to come after current word
         for bigram in smoothed_bigrams:
             # Set most likely word if this bigram has the highest probability found so far (25% chance it will not set)
-            if bigram[0] == current_word and smoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) > 1:
+            if bigram[0] == current_word[0] and smoothed_bigrams[bigram] > next_word["prob"] and random.randrange(1, 5) > 1:
                 next_word["prob"] = smoothed_bigrams[bigram]
                 next_word["word"] = bigram[1]
 
-        current_word = next_word["word"]
+        current_word = (next_word["word"], next_word["prob"])
         word_count += 1
     sentence += "</s>"
     print(sentence)
+    print("Sentence Probability (Log Space): ", sentence_p, "\n")
+
+#Trigram Model
+sentence_starters = []
+for trigram in unsmoothed_trigrams.most_common():
+    if trigram[0][0] == "<s>":
+        sentence_starters.append((trigram[0], trigram[1]))
+    if len(sentence_starters) == 30:
+        break
+    
+for i in range(10):
+    current_word = sentence_starters[random.randrange(0,20)]
+    current_word = (("<s>", "<s>", current_word[0][1]), current_word[1])
+    sentence = "<s> "
+    word_count = 0
+    sentence_p = 0
+    while current_word[0][2] != "</s>":
+        if word_count == 20:
+            break
+
+        sentence += current_word[0][2] + " "
+        sentence_p += log(current_word[1])
+        next_word = { "prob" : 0, "word": "" }
+        next_word_candidates = []
+        # Find most likely word to come after current word
+        for trigram in unsmoothed_trigrams:
+            # Set most likely word if this trigram has the highest probatrility found so far (25% chance it will not set)
+            # print(trigram)
+            if trigram[1] == current_word[0][2] and trigram[0] == current_word[0][1] and unsmoothed_trigrams[trigram] > next_word["prob"] and len(next_word_candidates) <= 5:
+                next_word_candidates.append({"prob": unsmoothed_trigrams[trigram], "word":trigram})
+
+        next_word = next_word_candidates[random.randrange(len(next_word_candidates))]
+        current_word = (next_word["word"], next_word["prob"])
+        # print(current_word)
+
+        word_count += 1
+    sentence += "</s>"
+    print(sentence)
+    print("Sentence Probability (Log Space): ", sentence_p, "\n")
+    
