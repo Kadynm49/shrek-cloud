1e152110a0022fc8d7da4b25e504e60a30335281
kadynm49 <marsha38@wwu.edu>
2020-06-02 15:08:24 -0700

    trifram, markov chain, and textgenrnn package

--- final/movie-reviewer.py
+++ final/movie-reviewer.py
@@ -7,13 +7,11 @@ import sys
 import json
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
-import numpy
+import numpy as np
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Dropout, LSTM
-# from tensorflow.keras.utils import to_categorical
-# from tensorflow.keras.callbacks import ModelCheckpoint
+from nltk.util import ngrams
+
 
 from nltk.corpus import reuters
 from nltk import bigrams, trigrams
@@ -62,7 +60,7 @@ def main():
     # those reviews to the list. Stop when there is no 
     # longer a "Load More" button
     reviews = []
-    print ("Fetching reviews from IMDB\n")
+    print ("\n==================================\nFetching reviews from IMDB\n==================================")
     while (load_more_button != None):
         review_containers = soup.find_all("div", class_="show-more__control")
         for review_container in review_containers:
@@ -93,14 +91,13 @@ def main():
     # All reviews in one string
     reviews_string = " ".join(reviews)
 
-    # Uncomment for tensorflow review (not working well and takes a long time to run)
-    # processed_inputs_string  = tokenize_words(reviews_string)
-    # create_review_with_tensorflow(processed_inputs, avg_score)
+    print("I rate " + title + " %.1f/10\n" % avg_score)
 
-    create_review_with_markov_chains(reviews_string.split(), avg_review_len, avg_score, title)
-    
-    # Uncomment for n grams review
-    # create_review_with_n_grams(reviews_string)
+    create_review_with_markov_chains(reviews_string.split(), avg_review_len, title)
+
+    # create_review_with_tensorflow(reviews)
+
+    create_review_with_n_grams(reviews_string.split())
 
 ##########################################
 # Markov chain generated text source 
@@ -110,9 +107,9 @@ def make_pairs(corpus):
         yield (corpus[i], corpus[i+1])
 
 # Source: https://towardsdatascience.com/simulating-text-with-markov-chains-in-python-1a27e6d13fc6
-def create_review_with_markov_chains(corpus, review_len, rating, title):
-    import numpy as np
-            
+def create_review_with_markov_chains(corpus, review_len, title):
+    print("\n==================================\nGenerating review with Markov chains\n")
+
     pairs = make_pairs(corpus)
 
     word_dict = {}
@@ -133,151 +130,86 @@ def create_review_with_markov_chains(corpus, review_len, rating, title):
 
     i = 0
     while True:
-        next_word = np.random.choice(word_dict[chain[-1]])
+        if (chain[-1] in word_dict): 
+            next_word = np.random.choice(word_dict[chain[-1]])
+        else: 
+            next_word = np.random.choice(corpus)
         chain.append(next_word)
         if i >= review_len and (next_word[-1] == '.' or next_word[-1] == '!' or next_word[-1] == '?'):
             break
         i += 1
 
     review = ' '.join(chain)
-
-    print("I rate " + title + " %.1f/10\n" % rating)
-    print(review)
+    print(review + "\n")
 
 ##########################################
 # N grams generated text source 
 # script from https://www.analyticsvidhya.com/blog/2019/08/comprehensive-guide-language-model-nlp-python-code/
-def create_review_with_n_grams(reviews_string):
-    from math import log
-    # Create a placeholder for model
-    model = defaultdict(lambda: defaultdict(lambda: 0))
-
-    # Count frequency of co-occurance  
-    for w1, w2, w3 in trigrams(reviews_string, pad_right=True, pad_left=True):
-        model[(w1, w2)][w3] += 1
+def create_review_with_n_grams(corpus): 
+    print("\n==================================\nGenerating review with trigrams\n")
+    trigrams = list(ngrams(corpus, 3))
+    unsmoothed_trigrams = Counter(ngrams(corpus, 3))
     
-    # Let's transform the counts to probabilities
-    for w1_w2 in model:
-        total_count = float(sum(model[w1_w2].values()))
-        for w3 in model[w1_w2]:
-            model[w1_w2][w3] = log(model[w1_w2][w3]/total_count)
-    
-    # code courtesy of https://nlpforhackers.io/language-models/
+    trigram_count = len(unsmoothed_trigrams)
 
-    # starting words
-    text = ["Shrek"]
-    sentence_finished = False
+    for trigram in unsmoothed_trigrams:
+        unsmoothed_trigrams[trigram] /= trigram_count
     
-    while not sentence_finished:
-        # select a random probability threshold  
-        r = random.random()
-        accumulator = .0
+    randint = np.random.randint(trigram_count)
+    
+    while trigrams[randint][2].islower():
+        randint = np.random.randint(trigram_count)
 
-        for word in model[tuple(text[-2:])].keys():
-            accumulator += model[tuple(text[-2:])][word]
-            # select words that are above the probability threshold
-            if accumulator >= r:
-                text.append(word)
+    first_word = (trigrams[randint], unsmoothed_trigrams[trigrams[randint]])
+
+    review = ""
+    # Generate sentences using unsmoothed trigram model
+    for i in range(10):
+        randint = np.random.randint(trigram_count)
+        
+        while trigrams[randint][2].islower():
+            randint = np.random.randint(trigram_count)
+
+        first_word = (trigrams[randint], unsmoothed_trigrams[trigrams[randint]])
+
+        current_word = first_word
+        sentence = ""
+        word_count = 0
+        while current_word[0][2][-1] != ".":
+            if word_count == 20:
                 break
 
-        if text[-2:] == [None, None]:
-            sentence_finished = True
-        
-        print (' '.join([t for t in text if t]))
-        
+            sentence += current_word[0][2] + " "
+            next_word = { "prob" : 0, "word": "" }
+            next_word_candidates = []
+            # Find most likely word to come after current word
+            for trigram in unsmoothed_trigrams:
+                # Set most likely word if this trigram has the highest probability found so far
+                if trigram[1] == current_word[0][2] and trigram[0] == current_word[0][1] and unsmoothed_trigrams[trigram] > next_word["prob"]:
+                    next_word["prob"] = unsmoothed_trigrams[trigram]
+                    next_word_candidates.append({"prob": unsmoothed_trigrams[trigram], "word":trigram})
+
+            next_word_candidates = sorted(next_word_candidates, key = lambda i: i['prob']) 
+            if (len(next_word_candidates) >= 5):
+                next_word = next_word_candidates[random.randrange(0, 5)]
+            else:
+                next_word = next_word_candidates[random.randrange(0, len(next_word_candidates))]        
+            current_word = (next_word["word"], next_word["prob"])
+
+            word_count += 1
+        sentence += current_word[0][2] + " "
+        review += sentence
+    
+    print(review + "\n")
+
 ##########################################
-# Machine learning generated text source 
-# https://stackabuse.com/text-generation-with-python-and-tensorflow-keras/
-def tokenize_words(input):
-    # lowercase everything to standardize it
-    input = input.lower()
-
-    # instantiate the tokenizer
-    tokenizer = RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(input)
-
-    # if the created token isn't in the stop words, make it part of "filtered"
-    filtered = filter(lambda token: token not in stopwords.words('english'), tokens)
-    return " ".join(filtered)
-
-def create_review_with_tensorflow(processed_inputs, rating):
-    chars = sorted(list(set(processed_inputs)))
-    char_to_num = dict((c, i) for i, c in enumerate(chars))
-
-    input_len = len(processed_inputs)
-    vocab_len = len(chars)
-    print ("Total number of characters:", input_len)
-    print ("Total vocab:", vocab_len)
-
-    seq_length = 100
-    x_data = []
-    y_data = []
-
-    # loop through inputs, start at the beginning and go until we hit
-    # the final character we can create a sequence out of
-    for i in range(0, input_len - seq_length, 1):
-        # Define input and output sequences
-        # Input is the current character plus desired sequence length
-        in_seq = processed_inputs[i:i + seq_length]
-
-        # Out sequence is the initial character plus total sequence length
-        out_seq = processed_inputs[i + seq_length]
-
-        # We now convert list of characters to integers based on
-        # previously and add the values to our lists
-        x_data.append([char_to_num[char] for char in in_seq])
-        y_data.append(char_to_num[out_seq])
-
-    n_patterns = len(x_data)
-    print ("Total Patterns:", n_patterns)
-
-    X = numpy.reshape(x_data, (n_patterns, seq_length, 1))
-    X = X/float(vocab_len)
-
-    y = to_categorical(y_data)
-
-    model = Sequential()
-    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(256, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(128))
-    model.add(Dropout(0.2))
-    model.add(Dense(y.shape[1], activation='softmax'))
-
-    # If weight file doesn't exist
-    # model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-    # filepath = title + "_model_weights_saved.hdf5"
-    # checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-    # desired_callbacks = [checkpoint]
-
-    # model.fit(X, y, epochs=20, batch_size=256, callbacks=desired_callbacks)
-
-    filename = title + "_model_weights_saved.hdf5"
-    model.load_weights(filename)
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-
-    num_to_char = dict((i, c) for i, c in enumerate(chars))
-
-    start = numpy.random.randint(0, len(x_data) - 1)
-    pattern = x_data[start]
-    print("Random Seed:")
-    print("\"", ''.join([num_to_char[value] for value in pattern]), "\"")
-
-    print("I rate this movie a %.1f/10" % rating)
-    for i in range(1000):
-        x = numpy.reshape(pattern, (1, len(pattern), 1))
-        x = x / float(vocab_len)
-        prediction = model.predict(x, verbose=0)
-        index = numpy.argmax(prediction)
-        result = num_to_char[index]
-        seq_in = [num_to_char[value] for value in pattern]
-
-        sys.stdout.write(result)
-
-        pattern.append(index)
-        pattern = pattern[1:len(pattern)]
+# Neural network text generation 
+# https://github.com/minimaxir/textgenrnn
+def create_review_with_tensorflow(reviews):
+    print("\n==================================\nGenerating review with a neural network\n")
+    from textgenrnn import textgenrnn
+    textgen = textgenrnn()
+    textgen.train_on_texts(reviews, num_epochs=1)
 
 if __name__ == '__main__': 
     main()
